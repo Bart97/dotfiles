2598eaf39af7441ef4e8fcdf80432364509d72ca
Bartosz Kaźmierczak <kazmierczakbartosz97@gmail.com>
2021-11-11 22:23:09 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Installing required packages"
+sudo pacman -Syu --needed stow qtile alacritty neovim cowsay dunst picom rofi arandr autorandr
+
+echo "Creating symlinks"
+stow alacritty
+stow dunst
+stow nvim
+stow picom
+stow qtile
+stow rofi
+
+cowsay "Done"
+
