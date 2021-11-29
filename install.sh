62d02b63677acd91f70c7b85d6c6ae0a0ec91d82
Bartosz Kazmierczak <bartosz.kazmierczak@nokia.com>
2021-11-29 08:35:06 +0100

    Add vimplug and minor tweaks
    Update Rofi config to always use Alacritty,
    Add missing network-manager dependency
    update .gitignore

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ sudo pacman -Syu --needed \
 	dunst \
 	neovim \
 	networkmanager \
+	network-manager-applet \
 	nitrogen \
 	picom \
 	python-dbus-next \
@@ -27,6 +28,11 @@ stow nvim
 stow picom
 stow qtile
 stow rofi
+
+#echo "Installing vim-plug"
+#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 
 cowsay "Done"
 
--- nvim/.config/nvim/init.vim
+++ nvim/.config/nvim/init.vim
@@ -1,5 +1,7 @@
 call plug#begin('~/.config/nvim/plugged')
     Plug 'lambdalisue/suda.vim'
+    Plug 'inkarkat/vim-mark'
+    Plug 'inkarkat/vim-ingo-library'
 call plug#end()
 
 set number
