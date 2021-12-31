fc71def43b886b72e6d96f1c2d02f35ed416780d
Bartosz Kazmierczak <kazmierczakbartosz97@gmail.com>
2021-12-31 23:23:49 +0100

    Update installer

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ sudo pacman -Syu --needed \
 	blueman \
 	cowsay \
 	dunst \
+    neofetch \
 	neovim \
 	networkmanager \
 	network-manager-applet \
@@ -30,9 +31,9 @@ stow picom
 stow qtile
 stow rofi
 
-#echo "Installing vim-plug"
-#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-#	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+echo "Installing vim-plug"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 
 cowsay "Done"
