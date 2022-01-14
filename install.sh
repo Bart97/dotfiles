1a6b9dc8ffe8b30a7ba3b3fb801a09cf79427fb7
Bartosz Kazmierczak <kazmierczakbartosz97@gmail.com>
2022-01-14 23:50:05 +0100

    Update install script
    Add acpilight instead of xbacklight
    Set Alacritty as default terminal
    Install Thunar and plugins

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 echo "Installing required packages"
 sudo pacman -Syu --needed \
+    acpilight \
 	alacritty \
 	arandr \
 	autorandr \
@@ -20,8 +21,11 @@ sudo pacman -Syu --needed \
 	qtile \
 	rofi \
 	stow \
+    thunar
+    thunar-archive-plugin
+    thunar-media-tags-plugin
+    thunar-volman
 	ttf-jetbrains-mono\
-	xorg-xbacklight
 
 echo "Creating symlinks"
 stow alacritty
@@ -35,6 +39,16 @@ echo "Installing vim-plug"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+echo "Setting Alacritty as the default terminal"
+gsettings set org.gnome.desktop.default-applications.terminal exec-arg '-e'
+gsettings set org.gnome.desktop.default-applications.terminal exec 'alacritty'
+# Ugly hack to force GLib to detect Alacritty as one of their hardcoded terminals
+# https://github.com/frida/glib/blob/main/gio/gdesktopappinfo.c
+sudo ln -s /bin/alacritty /bin/tilix
+
+echo "Configuring acpilight permissions"
+sudo cp ./90-backlight.rules /etc/udev/rules.d/90-backlight.rules
+sudo usermod -a -G video $USER
 
 cowsay "Done"
 
