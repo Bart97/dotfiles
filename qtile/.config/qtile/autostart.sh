81c1429db93078488e98b59e803551cdadf68f05
Bartosz Kazmierczak <bartosz.kazmierczak@nokia.com>
2022-03-30 07:54:05 +0200

    Add playerctld to startup

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -8,6 +8,7 @@ nitrogen --restore
 dunst &
 test -x /usr/lib/pam_kwallet_init && /usr/lib/pam_kwallet_init &
 mpris-proxy &
+playerctld daemon
 command -v telegram-desktop &> /dev/null && telegram-desktop -startintray &
 command -v signal-desktop&> /dev/null && signal-desktop --start-in-tray &
 flameshot &
