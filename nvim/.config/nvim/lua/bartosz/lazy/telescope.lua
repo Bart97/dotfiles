5b2f54aad8245727f17a936816b920f8b5af0200
Bartosz Kazmierczak <kazmierczakbartosz97@gmail.com>
2024-06-25 23:03:38 +0200

    Add smart-open.nvim

--- nvim/.config/nvim/lua/bartosz/lazy/telescope.lua
+++ nvim/.config/nvim/lua/bartosz/lazy/telescope.lua
@@ -1,13 +1,31 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        local builtin = require("telescope.builtin")
-        vim.keymap.set('n', '<leader>pf', builtin.find_files)
-        vim.keymap.set('n', '<C-p>', builtin.git_files)
-        vim.keymap.set('n', '<leader>pg', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") });
-        end)
-    end
-
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.6',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set('n', '<C-p>', builtin.git_files)
+            vim.keymap.set('n', '<leader>pg', function()
+                builtin.grep_string({ search = vim.fn.input("Grep > ") });
+            end)
+        end
+    },
+    {
+        "danielfalk/smart-open.nvim",
+        branch = "0.2.x",
+        config = function()
+            require("telescope").load_extension("smart_open")
+            vim.keymap.set('n', '<leader>pf', function()
+                require("telescope").extensions.smart_open.smart_open({ cwd_only = true, filename_first = false })
+            end)
+        end,
+        dependencies = {
+            "kkharji/sqlite.lua",
+            -- Only required if using match_algorithm fzf
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+            -- Optional.  If installed, native fzy will be used when match_algorithm is fzy
+            { "nvim-telescope/telescope-fzy-native.nvim" },
+        },
+    }
 }
