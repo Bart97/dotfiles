9291e152fc305a23156b2cad600ddd7c824e5580
Bartosz Kazmierczak <bartosz.kazmierczak@nokia.com>
2024-06-26 10:43:48 +0200

    Add rainbow-delimiters.nvim and gitsigns

--- nvim/.config/nvim/lua/bartosz/lazy/init.lua
+++ nvim/.config/nvim/lua/bartosz/lazy/init.lua
@@ -2,8 +2,9 @@ return {
     {
         'inkarkat/vim-mark',
         dependencies = 'inkarkat/vim-ingo-library'
-    }, 
+    },
     'mtdl9/vim-log-highlighting',
     'wilriker/gcode.vim',
-    'nvim-tree/nvim-web-devicons'
+    'nvim-tree/nvim-web-devicons',
+    'HiPhish/rainbow-delimiters.nvim',
 }
