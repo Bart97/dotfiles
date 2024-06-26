cbf1984aefedbd4aff0f09bf3a262e40f1ff6faf
Bartosz Kazmierczak <bartosz.kazmierczak@nokia.com>
2024-06-26 10:40:34 +0200

    Use OSC52 in nvim if it's running in an SSH session

--- nvim/.config/nvim/lua/bartosz/configs.lua
+++ nvim/.config/nvim/lua/bartosz/configs.lua
@@ -6,3 +6,18 @@ vim.opt.list = true
 vim.opt.listchars["tab"] = '▸'
 vim.opt.listchars["trail"] = '·'
 vim.opt.number = true
+
+if vim.env.SSH_TTY ~= nil then
+    -- Workaroud because Alacritty doesn't play nice with neovim by default
+    vim.g.clipboard = {
+        name = 'OSC 52',
+        copy = {
+            ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+            ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+        },
+        paste = {
+            ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+            ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+        },
+    }
+end
