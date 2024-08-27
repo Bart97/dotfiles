1832521499bbf2946ab169c0afaedc7e78521b8d
Bartosz Kazmierczak <bartosz.kazmierczak@nokia.com>
2024-08-27 14:53:38 +0200

    [nvim] add nvim-treesitter-context

--- nvim/.config/nvim/lua/bartosz/lazy/treesitter.lua
+++ nvim/.config/nvim/lua/bartosz/lazy/treesitter.lua
@@ -1,16 +1,29 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })()
-    end,
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = { "c", "cpp", "python", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "yaml" },
-            sync_install = false,
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },  
-            additional_vim_regex_highlighting = false
-        })
-    end
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = function()
+            require("nvim-treesitter.install").update({ with_sync = true })()
+        end,
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "c", "cpp", "python", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "yaml" },
+                sync_install = false,
+                auto_install = true,
+                highlight = { enable = true },
+                indent = { enable = true },
+                additional_vim_regex_highlighting = false
+            })
+        end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        config = function()
+            require("treesitter-context").setup {
+                mode = "topline",
+                max_lines = 2,
+                trim_scope = "inner",
+                multiline_threshold = 1
+            }
+        end
+    }
 }
